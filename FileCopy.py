cba7dd3cb9a1aebf124d8e7ff2db75026419c765
bicho <bichojunk@hotmail.com>
2018-01-12 23:51:18 -0700

    Enhanced file copy stuff and added JSON output.

--- FileCopy.py
+++ FileCopy.py
@@ -1,20 +1,40 @@
-import os
+import os, json
 from shutil import disk_usage, copy
 
-mypath = "/home/pi"
+mypath = "/home/brent/Desktop"
 stuffStats = disk_usage(mypath)
 
 print(stuffStats)
 
-fileSrc = "/home/pi/SingleFileTest/messy/test.txt"
-dirDest = "/home/pi/SingleFileTest/cleanup"
+fileSrc = "/home/brent/Desktop/SingleFileTest/messy/test.txt"
+dirDest = "/home/brent/Desktop/SingleFileTest/cleanup"
 
-fileparts = os.path.split(fileSrc)
+path = os.path.split(fileSrc)
+name, ext = os.path.splitext(path[1])
 
 print("dirDest ", dirDest)
-print(fileparts[1])
-fileDest = os.path.join(dirDest, fileparts[1])
+print("Source Path:", path[0])
+print("Source File:", path[1])
+print("name: ", name)
+print("ext: ", ext)
+
+fileDest = os.path.join(dirDest, ext)
 print("My directory: ", fileDest)
+
+# Output to File
+data = [{
+    'source': fileSrc,
+    'destination': dirDest,
+    'extension': ext
+},
+    {
+        'source': "My Source",
+        'destination': "My Destination",
+        'extension': "My Extension"
+    }]
+
+with open(dirDest + "/data.txt", "w") as outfile:
+    json.dump(data, outfile)
 
 if not (os.path.exists(fileDest)):
     copy(fileSrc, fileDest)
